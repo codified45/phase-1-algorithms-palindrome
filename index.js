7c3cc85ff0f89c46d225cc1170a8636b231de3d5
Jack Lach <jack.lach4@gmail.com>
2023-03-18 19:50:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  let isPalindrome = false;
+  let ltrArray = [...word];
+  let ltrReversed = [];
+  for (const letter of ltrArray) {
+    ltrReversed.unshift(letter);
+  };
+  if (ltrArray.toString() === ltrReversed.toString()) {
+    isPalindrome = true;
+  };
+  return isPalindrome;
+};
 
 /* 
   Add your pseudocode here
@@ -20,6 +30,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
-}
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("bait"));
+};
 
 module.exports = isPalindrome;
